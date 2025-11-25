30f5001778c899a4e09f79c6b5dcf4d31e5d3181
lukasz04prz <lukasz04prz@gmail.com>
2025-11-25 08:02:47 +0000

    Com4_1.1

--- index.js
+++ index.js
@@ -6,16 +6,23 @@ const app = express();
 // define endpoint for exercise 1 here
 app.get('/math/circle/:r', (req, res) => {
 //TODO1  
+  const r = parseFloat(req.params.r);
+
+  if (isNaN(r))
+  {return res.status(400).json({ error: 'Promień musi być liczbą' });}
+
+  const area = (Math.PI * Math.pow(r, 2)).toFixed(2);
+  const circumference = (2 * Math.PI * r).toFixed(2);
+
+  const result = {
+    area: area,
+    circumference: circumference
+  }
+
   res.json(result);
 });
 
-//TODO2
-
-
-//TODO3
-
-
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
