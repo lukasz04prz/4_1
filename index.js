4d6dae05a0e2bb869d64e949dc5cbec4c280b465
lukasz04prz <lukasz04prz@gmail.com>
2025-11-25 08:04:44 +0000

    Com4_1.2

--- index.js
+++ index.js
@@ -22,6 +22,26 @@ app.get('/math/circle/:r', (req, res) => {
   res.json(result);
 });
 
+//TODO2
+app.get('/math/rectangle/:a/:b', (req, res) => {
+
+  const a = parseFloat(req.params.a);
+  const b = parseFloat(req.params.b);
+
+  if (isNaN(a) || isNaN(b))
+  {return res.status(400).json({ error: 'Wysokość i szerokość musza być liczbami' });}
+
+  const area = (a*b).toFixed(2);
+  const perimeter = 2 * (a + b).toFixed(2);
+
+  const result = {
+    area: area,
+    perimeter: perimeter
+  }
+
+  res.json(result);
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
